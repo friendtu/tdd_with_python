16a51f26680f71d3d1063751cca7e512c8164c7d
friendtu <friendtu@gmail.com>
2019-09-04 21:39:39 +0800

    Front page html now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -12,7 +12,8 @@ class HomePageTest (TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        response = self.client.get('/')
+        response = home_page(request)
+        #response = self.client.get('/')
         #html = response.content.decode('utf8').strip()
         #self.assertTrue(html.startswith('<html>'))
         #self.assertIn('<title>To-Do lists</title>',html)
