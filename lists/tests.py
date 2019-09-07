9201711c492096a22493fb54c2844464fb0a0489
Donnie Tu <friendtu@gmail.com>
2019-09-07 11:39:02 +0800

    add two inputs

--- lists/tests.py
+++ lists/tests.py
@@ -19,3 +19,8 @@ class HomePageTest (TestCase):
         #self.assertTrue(html.endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_post_request(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
