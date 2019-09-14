8e2bd964b0d04d03c00de12f2197dc4be079f639
friendtu <friendtu@gmail.com>
2019-09-14 19:25:53 +0800

    add list model

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -11,6 +11,7 @@ def view_list(request):
     return render(request, 'list.html', {'items': Item.objects.all()})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_=List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],list=list_)
     return redirect('/lists/the_only_list/')
 
