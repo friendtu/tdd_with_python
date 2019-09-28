a60dff4b2cd4eb31f1c465b89f95a8d52cc72549
Donnie Tu <friendtu@gmail.com>
2019-09-29 07:34:21 +0800

    add widget for forms

--- lists/forms.py
+++ lists/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 class ItemForm(forms.Form):
-    item_text=forms.CharField()
+    item_text=forms.CharField(
+        widget=forms.fields.TextInput(
+            attrs={
+                'placeholder':'Enter a to-do item',
+                'class':'form-control input-lg'
+                }
+        )
+    )
