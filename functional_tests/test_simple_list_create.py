2eda86fe4b2ccc95719bd3a138b427eec7cd5f41
Donnie Tu <friendtu@gmail.com>
2019-09-30 18:28:48 +0800

    rename all item input ids and names. still broken

--- functional_tests/test_simple_list_create.py
+++ functional_tests/test_simple_list_create.py
@@ -14,7 +14,7 @@ class NewVisitorTest(FunctionTest):
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
 
-        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox=self.get_item_input_box()
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('Buy peacock feathers')
@@ -23,7 +23,7 @@ class NewVisitorTest(FunctionTest):
 
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -35,7 +35,7 @@ class NewVisitorTest(FunctionTest):
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
-        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox=self.get_item_input_box()
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
@@ -52,7 +52,7 @@ class NewVisitorTest(FunctionTest):
         page_text=self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertNotIn('make a fly',page_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Buy milk")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy milk")
