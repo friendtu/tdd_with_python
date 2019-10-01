01f528b211a275212890ba479f7a58753c0f30fc
friendtu <friendtu@gmail.com>
2019-10-01 09:33:39 +0800

    use form.save

--- lists/forms.py
+++ lists/forms.py
@@ -14,3 +14,6 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+    def save(self,for_list):
+        self.instance.list=for_list
+        return super().save()
