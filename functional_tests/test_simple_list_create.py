32088dad2afdcc97cd49fbf863319b190f123fd6
friendtu <friendtu@gmail.com>
2020-06-11 20:43:56 +0800

    test jenkins

--- functional_tests/test_simple_list_create.py
+++ functional_tests/test_simple_list_create.py
@@ -63,4 +63,5 @@ class NewVisitorTest(FunctionTest):
         page_text=self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertIn('Buy milk',page_text)
+#
 
